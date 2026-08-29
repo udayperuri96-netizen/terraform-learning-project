d68dac1480d8ffd9e1432c7d16ee368289c2f4f0
udaysekhar <udaysekhar@udaysekhar-ka-MacBook-Pro.local>
2026-08-29 23:25:46 +0530

    Fix terraform formatting

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
